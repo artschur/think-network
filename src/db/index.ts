4602a3dc599a3a3a5b6ac6d84cd956f241caed79
Arthur Schurhaus <arthurschur@icloud.com>
2025-06-25 10:29:43 -0300

    fixed database connection error

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, {
+  prepare: false,
+});
+export const db = drizzle(client);
 
 export const clerkClient = createClerkClient({
   secretKey: process.env.CLERK_SECRET_KEY,
@@ -13,4 +16,3 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
 );
-export const db = drizzle(client);
